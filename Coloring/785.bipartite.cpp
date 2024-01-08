ef40e0364d8c9f34756a1e8a3d528e592ce393d9
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-01-08 23:53:55 +0530

    785, 802

---
+++ Coloring/785.bipartite.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Leetcode 785 - Is Graph Bipartite?
+        Given an undirected graph, return true if it is bipartite, false otherwise.
+        A graph is bipartite if we can divide the vertices into two disjoint sets such that all edges are between the two sets.
+
+        Example:
+            [[1,3],
+             [0,2],
+             [1,3],
+             [0,2]]
+            ans = true
+
+        We do a BFS/DFS from all the unvisited vertices. If we encounter a vertex that is already visited, then we check if it is of the same color as the current vertex.
+        If it is, then the graph is not bipartite. Else, we continue.
+        If we encounter a vertex that is not visited, then we mark it with the opposite color of the current vertex and continue.
+
+        Time: O(n+m), Space: O(n)
+*/
+
+static int color[101];
+class Solution {
+public:
+    bool dfs(const vector<vector<int>>& graph, int i, int prev, int color[]) {
+        color[i] = prev;
+
+        for (auto& v : graph[i]) {
+            if (color[v] == -1) {
+                if (!dfs(graph, v, !prev, color)) return false;
+            }
+            else if (color[v] == color[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool bfs(const vector<vector<int>>& graph, int start, int color[]) {
+        queue<int> que;
+        que.push(start);
+        color[start] = 0;
+
+        while (!que.empty()) {
+            int i = que.front();
+            que.pop();
+
+            for (auto& v : graph[i]) {
+                if (color[v] == -1) {
+                    color[v] = !color[i];
+                    que.push(v);
+                }
+                else if (color[v] == color[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        memset(color, -1, sizeof(int) * 101);
+
+        for (int i = 0; i < graph.size(); i++) {
+            if (color[i] == -1) {
+                if (!bfs(graph, i, color)) return false;
+                // if(!dfs(graph, i, 0, color)) return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> graph = { {1,3},{0,2},{1,3},{0,2} };
+    cout << s.isBipartite(graph) << endl;
+    return 0;
+}
---
+++ Topological/802.findEventualSafeStates.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+    Leetcode 802 - Find Eventual Safe States
+        Given a directed graph, return all the vertices that are not part of a cycle.
+        A vertex is part of a cycle if we can reach it from itself.
+        A vertex is not part of a cycle if we can't reach it from itself.
+
+        We do a DFS from all the unvisited vertices. We now check if its safe by doing a DFS from all the vertices that are reachable from it.
+        If we encounter a vertex that is already visited, then we check if it is part of the current path. If it is, then the vertex is not safe.
+        Else, we continue.
+
+        Finally set the vertex as safe and unset it as part of the current path.
+
+        Once all the vertices are visited, we return all the vertices that are safe.
+
+        Time: O(n+m), Space: O(n)
+*/
+
+class Solution {
+    bool dfs(const vector<vector<int>>& graph, int i,
+        vector<bool>& visited, vector<bool>& path, vector<bool>& safe) {
+
+        path[i] = visited[i] = 1;
+
+        for (auto& v : graph[i]) {
+            if (visited[v] && safe[v]) continue;
+            else if (!visited[v]) {
+                if (dfs(graph, v, visited, path, safe)) return true;
+            }
+            else if (path[v]) {
+                return true;
+            }
+        }
+
+        path[i] = 0;
+        safe[i] = 1;
+        return false;
+    }
+
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<bool> safe(n, 0), visited(n, 0), path(n, 0);
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                dfs(graph, i, visited, path, safe);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (safe[i]) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> graph = { {1,2},{2,3},{5},{0},{5},{},{} };
+    vector<int> ans = s.eventualSafeNodes(graph);
+    for (auto& x : ans) cout << x << " ";
+    cout << endl;
+    return 0;
+}
+
