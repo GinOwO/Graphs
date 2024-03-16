ee4b9c7a205398ebb6851821298d7efe1461b970
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-03-17 00:14:11 +0530

    mcolorability

--- Coloring/785.bipartite.cpp
+++ Coloring/785.bipartite.cpp
@@ -21,38 +21,38 @@ using namespace std;
         Time: O(n+m), Space: O(n)
 */
 
-static int color[101];
+static int color [ 101 ];
 class Solution {
 public:
-    bool dfs(const vector<vector<int>>& graph, int i, int prev, int color[]) {
-        color[i] = prev;
+    bool dfs( const vector<vector<int>>& graph, int i, int prev, int color[] ) {
+        color [ i ] = prev;
 
-        for ( auto& v : graph[i] ) {
-            if ( color[v] == -1 ) {
-                if ( !dfs(graph, v, !prev, color) ) return false;
+        for ( auto& v : graph [ i ] ) {
+            if ( color [ v ] == -1 ) {
+                if ( !dfs( graph, v, !prev, color ) ) return false;
             }
-            else if ( color[v] == color[i] ) {
+            else if ( color [ v ] == color [ i ] ) {
                 return false;
             }
         }
         return true;
     }
 
-    bool bfs(const vector<vector<int>>& graph, int start, int color[]) {
+    bool bfs( const vector<vector<int>>& graph, int start, int color[] ) {
         queue<int> que;
-        que.push(start);
-        color[start] = 0;
+        que.push( start );
+        color [ start ] = 0;
 
-        while ( !que.empty() ) {
-            int i = que.front();
-            que.pop();
+        while ( !que.empty( ) ) {
+            int i = que.front( );
+            que.pop( );
 
-            for ( auto& v : graph[i] ) {
-                if ( color[v] == -1 ) {
-                    color[v] = !color[i];
-                    que.push(v);
+            for ( auto& v : graph [ i ] ) {
+                if ( color [ v ] == -1 ) {
+                    color [ v ] = !color [ i ];
+                    que.push( v );
                 }
-                else if ( color[v] == color[i] ) {
+                else if ( color [ v ] == color [ i ] ) {
                     return false;
                 }
             }
@@ -60,12 +60,12 @@ public:
         return true;
     }
 
-    bool isBipartite(vector<vector<int>>& graph) {
-        memset(color, -1, sizeof(int) * 101);
+    bool isBipartite( vector<vector<int>>& graph ) {
+        memset( color, -1, sizeof( int ) * 101 );
 
-        for ( int i = 0; i < graph.size(); i++ ) {
-            if ( color[i] == -1 ) {
-                if ( !bfs(graph, i, color) ) return false;
+        for ( int i = 0; i < graph.size( ); i++ ) {
+            if ( color [ i ] == -1 ) {
+                if ( !bfs( graph, i, color ) ) return false;
                 // if(!dfs(graph, i, 0, color)) return false;
             }
         }
@@ -73,9 +73,9 @@ public:
     }
 };
 
-int main() {
+int main( ) {
     Solution s;
     vector<vector<int>> graph = { {1,3},{0,2},{1,3},{0,2} };
-    cout << s.isBipartite(graph) << endl;
+    cout << s.isBipartite( graph ) << endl;
     return 0;
 }
